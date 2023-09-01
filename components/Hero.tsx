8b692441611f3fadfa45c05007e3161ba0a9f123
viannieab <123542222+viannieab@users.noreply.github.com>
2023-09-01 23:05:46 +0300

    initial commit

---
+++ components/Hero.tsx
@@ -0,0 +1,32 @@
+"use client";
+import Image from 'next/image'
+import CustomButton from './CustomButton'
+
+export default function Hero() {
+  const handleScroll =() =>{
+
+  }
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero_title">
+          Find, book or rent a car -- quickly and easily! 
+        </h1>
+        <p className="hero_subtitle">
+          Streamline your car rental experience with our effortless booking process.
+        </p>
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+        <div className="hero_image-container">
+            <div className="hero_image">
+              <Image src="/hero.png" alt="hero" fill className="object-contain"/>
+            </div>
+            <div className="hero_image-overlay"/>
+        </div>
+    </div>
+  )
+}
---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: [
+            'cdn.imagin.studio','cars-by-api-ninjas.p.rapidapi.com'
+        ]
+    }
+}
+
+module.exports = nextConfig
